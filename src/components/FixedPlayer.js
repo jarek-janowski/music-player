2bc63007f5c6fdc8384e915d04361a3dda9d6615
jarek-janowski <jarjanowski95@gmail.com>
2021-03-23 20:48:36 +0100

    feat: the final touches in styles

--- src/components/FixedPlayer.js
+++ src/components/FixedPlayer.js
@@ -14,17 +14,11 @@ const FixedPlayer = ({
 }) => {
     const { album, title, artist } = song
 
-    // const colorAll = currentPlaylist === "all" ? "#009FFD" : "#EAF6FF";
-    // const colorFavourites = currentPlaylist === "favourites" ? "#009FFD" : "#EAF6FF";
-    // const styleAll = { colorAll };
-    // const styleFavourites = {colorFavourites}
-    
     const pauseIcon = <i className="fa fa-pause" aria-hidden="true"></i>
     const playIcon = <i className="fa fa-play" aria-hidden="true"></i>
     const emptyHeart = <i className="fa fa-heart-o" aria-hidden="true"></i>
     const filledHeart = <i className="fa fa-heart" aria-hidden="true"></i>
 
-    
     const currentPlaylistFavourites = currentPlaylist === "favourites"
     const currentPlaylistAll = currentPlaylist === "all"
     const favourites = currentPlaylistFavourites ? "playlists__chosen" : "playlists__no-chosen"
@@ -36,18 +30,18 @@ const FixedPlayer = ({
     return (
         <>
         <div className={`fixed-player ${className}`}>
-            <div onClick={() => window.scrollTo(0, 0)}>
+            <div >
                 <div ref={progressRef} className="fixed-player__progress-container" >
                     <div style={{width: (progress * 100) + '%'}}></div>
                 </div>
-                <img className="fixed-player__image" src={album.cover_medium} alt={`${title} cover`}/>
-                <div className="fixed-player__info">
-                <h3 className="fixed-player__title">{title}</h3>
-                <p className="fixed-player__artist">{artist.name}</p>
+                <img onClick={() => window.scrollTo(0, 0)} className="fixed-player__image" src={album.cover_medium} alt={`${title} cover`}/>
+                <div onClick={() => window.scrollTo(0, 0)} className="fixed-player__info">
+                    <h3 className="fixed-player__title">{title}</h3>
+                    <p className="fixed-player__artist">{artist.name}</p>
                 </div>
             </div>
-            <button className="fixed-player__heart"onClick={addRemoveFromFavourites}>{includes ? filledHeart : emptyHeart}</button>
-            <button className="fixed-player__button" onClick={handlePlayPause}>{isPaused ? playIcon : pauseIcon}</button>
+                <button className="fixed-player__heart"onClick={addRemoveFromFavourites}>{includes ? filledHeart : emptyHeart}</button>
+                <button className="fixed-player__button" onClick={handlePlayPause}>{isPaused ? playIcon : pauseIcon}</button>
             <div className="playlists">
                 <button className={all} onClick={playAll}>All songs <span>{currentPlaylistAll ? "" : playIcon}</span></button>
                 <button className={favourites} onClick={playFavourites}>Favourites <span>{currentPlaylistFavourites ? "" : playIcon}</span></button>
